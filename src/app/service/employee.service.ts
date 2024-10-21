dba6ca43e5bfc6c3a156c62721f0a1444c102e70
keith <keithcompere150@gmail.com>
2024-10-20 23:48:03 -0400

    I think this should work?

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable} from "rxjs";
 import {Employee} from "../model/employee";
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class EmployeeService {
+  private fireBase: Firestore
+  constructor(fireBase: Firestore){
+    this.fireBase = fireBase
+  }
   employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
 
-  get $(): Observable<readonly Employee[]> {
-    return this.employees$;
+  getEmployees(): Observable<Employee[]> {
+    const employees = collection(this.fireBase, 'employees');  // our collection of employees
+    return collectionData(employees, {idField: "id"}) as Observable<Employee[]>  // the collection data
   }
 
   addEmployee(employee: Employee) {
-    this.employees$.next([...this.employees$.getValue(), employee]);
-    return true;
+    return addDoc(collection(this.fireBase, "employees"), employee)   // adds employee to the firestore
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+export const environment = {
+    // Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig: {
+    apiKey: "AIzaSyBh11Jxdf9gZ-7UAzqpZLNQQZsBxs3qeqI",
+    authDomain: "lab5-9744a.firebaseapp.com",
+    projectId: "lab5-9744a",
+    storageBucket: "lab5-9744a.appspot.com",
+    messagingSenderId: "17892648884",
+    appId: "1:17892648884:web:12fe9ce0a661673951f4e1",
+    measurementId: "G-HZLX49EE3N"
+    }
+};
